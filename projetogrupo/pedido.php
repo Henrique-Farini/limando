4613000fb8c2dd2b2af4e4c8b63ed38537968d0b
Henrique-Farini <henrique.f.ferreira@aluno.senai.com.br>
2025-09-10 07:24:59 -0300

    10.09.25

---
+++ projetogrupo/pedido.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem</title>
+</head>
+    <body style=" background-image: url('eve.jpg'); background-size: cover; margin: 0; height: 100vh; display: flex; justify-content: center; align-items: center; background-color: #f1d0d0ff;">
+    <form style="width: 500px; border: 2px solid black; padding: 10px; box-shadow: 2px 2px 2px 2px; border-radius: 10px; background-color: #393939ff;" action="gravar_chamada.php" method="post">
+    
+
+
+
+</form>
+</body>
+</html> 
